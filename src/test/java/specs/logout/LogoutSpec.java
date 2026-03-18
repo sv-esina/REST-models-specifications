916cfaecdfff56589af95a2e6af37d9106a5f187
Esina Svetlana <montesum1@gmail.com>
2026-03-18 21:06:41 +0300

    added Logout Tests

---
+++ src/test/java/specs/logout/LogoutSpec.java
@@ -0,0 +1,37 @@
+package specs.logout;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+
+import static io.restassured.filter.log.LogDetail.ALL;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class LogoutSpec {
+
+    public static ResponseSpecification successfulLogoutResponseSpec = new ResponseSpecBuilder().
+            log(ALL).
+            expectStatusCode(200).
+            expectBody(matchesJsonSchemaInClasspath(
+                    "schemas/logout/successful_logout_response_schema.json")).
+            expectBody(equalTo("{}")).
+            build();
+
+    public static ResponseSpecification invalidLogoutResponseSpec = new ResponseSpecBuilder().
+            log(ALL).
+            expectStatusCode(401).
+            expectBody(matchesJsonSchemaInClasspath(
+                    "schemas/logout/invalid_token_logout_response_schema.json")).
+            expectBody("detail", notNullValue()).
+            expectBody("code", notNullValue()).
+            build();
+
+    public static ResponseSpecification wrongLogoutResponseSpec = new ResponseSpecBuilder().
+            log(ALL).
+            expectStatusCode(400).
+            expectBody(matchesJsonSchemaInClasspath(
+                    "schemas/logout/wrong_params_logout_response_schema.json")).
+            expectBody("refresh", notNullValue()).
+            build();
+}
---
+++ src/test/java/tests/LogoutTests.java
@@ -0,0 +1,112 @@
+package tests;
+
+import models.login.LoginBodyModel;
+import models.logout.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static specs.login.LoginSpec.*;
+import static specs.logout.LogoutSpec.*;
+import static specs.registration.RegistrationSpec.requestSpec;
+
+public class LogoutTests extends TestBase{
+    String username = "sv_esina";
+    String password = "password1";
+    String wrongRefreshToken = "wrongToken123";
+    String emptyRefreshToken = "";
+    String nullRefreshToken = null;
+
+    @Test
+    @DisplayName("Успешный выход из сессии")
+    public void successfulLogoutTest() {
+
+        String refreshToken = step("Авторизация и получение refresh токена", () -> {
+            LoginBodyModel loginData = new LoginBodyModel(username, password);
+            return given(loginRequestSpec)
+                    .body(loginData)
+                    .when()
+                    .post("/auth/token/")
+                    .then()
+                    .spec(successfulLoginResponseSpec)
+                    .extract().path("refresh");
+        });
+
+        step("Отправка запроса на выход из сессии", () -> {
+            LogoutBodyModel logoutBody = new LogoutBodyModel(refreshToken);
+            SuccessfulLogoutResponseModel successfulLogout = given(requestSpec)
+                    .body(logoutBody)
+                    .when()
+                    .post("/auth/logout/")
+                    .then()
+                    .spec(successfulLogoutResponseSpec)
+                    .extract().as(SuccessfulLogoutResponseModel.class);
+        });
+    }
+
+    @Test
+    @DisplayName("Выполнение запроса с некорректным токеном")
+    public void invalidTokenLogoutTest() {
+
+        InvalidTokenLogoutModel invalidToken =
+                step("Отправка запроса с некорректным токеном", () -> {
+            LogoutBodyModel logoutBody = new LogoutBodyModel(wrongRefreshToken);
+            return given(requestSpec)
+                    .body(logoutBody)
+                    .when()
+                    .post("/auth/logout/")
+                    .then()
+                    .spec(invalidLogoutResponseSpec)
+                    .extract().as(InvalidTokenLogoutModel.class);
+        });
+
+        step("Проверка текста полученных ошибок", () -> {
+            assertThat(invalidToken.detail()).isEqualTo("Token is invalid");
+            assertThat(invalidToken.code()).isEqualTo("token_not_valid");
+        });
+    }
+
+    @Test
+    @DisplayName("Выполнение запроса с null в параметре refresh")
+    public void nullTokenLogoutTest() {
+
+        EmptyOrNullParamLogoutResponseModel nullOrEmptyToken =
+                step("Отправка запроса с некорректным токеном", () -> {
+                    LogoutBodyModel logoutBody = new LogoutBodyModel(nullRefreshToken);
+                    return given(requestSpec)
+                            .body(logoutBody)
+                            .when()
+                            .post("/auth/logout/")
+                            .then()
+                            .spec(wrongLogoutResponseSpec)
+                            .extract().as(EmptyOrNullParamLogoutResponseModel.class);
+                });
+
+        step("Проверка текста полученных ошибок", () -> {
+            assertThat(nullOrEmptyToken.refresh().get(0)).isEqualTo("This field may not be null.");
+        });
+    }
+
+    @Test
+    @DisplayName("Выполнение запроса с пустым параметром refresh")
+    public void emptyTokenLogoutTest() {
+
+        EmptyOrNullParamLogoutResponseModel nullOrEmptyToken =
+                step("Отправка запроса с пустым параметром refresh", () -> {
+                    LogoutBodyModel logoutBody = new LogoutBodyModel(emptyRefreshToken);
+                    return given(requestSpec)
+                            .body(logoutBody)
+                            .when()
+                            .post("/auth/logout/")
+                            .then()
+                            .spec(wrongLogoutResponseSpec)
+                            .extract().as(EmptyOrNullParamLogoutResponseModel.class);
+                });
+
+        step("Проверка текста полученной ошибки", () -> {
+            assertThat(nullOrEmptyToken.refresh().get(0)).isEqualTo("This field may not be blank.");
+        });
+    }
+}
